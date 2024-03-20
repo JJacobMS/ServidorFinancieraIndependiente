ef062c661b5ca8e3dd0d95f643ecb16c6d11532d
engaherr <kikgamboa@gmail.com>
2024-03-19 19:00:30 -0600

    feat(Servicios y Host): Interfaz de servicio CondicionCredito + implementación añadidas con la inclusión en el endpoint

---
+++ ServiciosFinancieraIndependiente/ICondicionCredito.cs
@@ -0,0 +1,17 @@
+﻿using DatosFinancieraIndependiente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorFinancieraIndependiente
+{
+    [ServiceContract]
+    public interface ICondicionCredito
+    {
+        [OperationContract]
+        Codigo GuardarCondicionCredito(CondicionCredito condicionCredito);
+    }
+}
---
+++ ServiciosFinancieraIndependiente/ServiciosFinancieraIndependienteCondicionCredito.cs
@@ -0,0 +1,42 @@
+﻿using DatosFinancieraIndependiente;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorFinancieraIndependiente
+{
+    public partial class ServiciosFinancieraIndependiente : ICondicionCredito
+    {
+        public Codigo GuardarCondicionCredito(CondicionCredito condicionCredito)
+        {
+            Codigo codigo = Codigo.EXITO;
+
+            try
+            {
+                using (FinancieraBD context = new FinancieraBD())
+                {
+                    context.CondicionCredito.Add(condicionCredito);
+                    context.SaveChanges();
+                }
+            }
+            catch (SqlException ex)
+            {
+                //TODO Log
+                codigo = Codigo.ERROR_BD;
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (EntityException ex)
+            {
+                //TODO Log
+                codigo = Codigo.ERROR_BD;
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return codigo;
+        }
+    }
+}
