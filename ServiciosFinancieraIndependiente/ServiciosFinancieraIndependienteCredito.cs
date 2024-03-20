3aa3af15dc547e897cdc4382f8114ce0176d226b
engaherr <kikgamboa@gmail.com>
2024-03-20 15:23:35 -0600

    feat(Host y Servicios): Interfaz de comunicación e implementación de servidor para Crédito añadida con su endpoint

---
+++ ServiciosFinancieraIndependiente/ServiciosFinancieraIndependienteCredito.cs
@@ -0,0 +1,48 @@
+﻿using DatosFinancieraIndependiente;
+using ServidorFinancieraIndependiente;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorFinancieraIndependiente
+{
+    public partial class ServiciosFinancieraIndependiente : ICredito
+    {
+        private static readonly int ID_ESTATUS_PENDIENTE = 1;
+        private static readonly int ID_ESTATUS_APROBADO = 2;
+        private static readonly int ID_ESTATUS_RECHAZADO = 3;
+
+        public Codigo GuardarInformacionSolicitud(Credito credito)
+        {
+            Codigo codigo = Codigo.EXITO;
+
+            try
+            {
+                using (FinancieraBD contexto = new FinancieraBD())
+                {
+                    credito.EstatusCredito_idEstatusCredito = ID_ESTATUS_PENDIENTE;
+                    contexto.Credito.Add(credito);
+                    contexto.SaveChanges();
+                }
+            }
+            catch (SqlException ex)
+            {
+                //TODO Log
+                codigo = Codigo.ERROR_BD;
+                Console.WriteLine(ex.StackTrace + ex.Message);
+            }
+            catch (EntityException ex)
+            {
+                //TODO Log
+                codigo = Codigo.ERROR_BD;
+                Console.WriteLine(ex.StackTrace + ex.Message);
+            }
+
+            return codigo;
+        }
+    }
+}
